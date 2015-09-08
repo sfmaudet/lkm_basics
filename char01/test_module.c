4be979e69d74bee896954ea8cf2363477c99abda
sfmaudet <sfmaudet@gmail.com>
2015-09-08 18:25:51 -0300

    More comments

--- char01/test_module.c
+++ char01/test_module.c
@@ -1,3 +1,27 @@
+/* Copyright (C) 2015, Santiago F. Maudet
+ * This file is part of char01 module.
+ *
+ * char01 module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * char01 module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* !\brief
+*   This  is a user application to read data from the module buffer.
+*   USAGE: ./test_module /dev/char_01
+*/
+
+
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -13,6 +37,7 @@ int main (int argc, char ** argv){
 
   if(argc < 2){
     perror("usage: ./test_module /dev/[device_name]");
+    return -1;
   }
 
   fd = open(argv[1],O_RDONLY);
